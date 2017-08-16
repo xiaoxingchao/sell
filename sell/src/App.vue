7c4b51c1503019061e306c07af63b4e840f1d5c2
xxc <549021324@qq.com>
2017-08-16 23:22:16 +0800

    头

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header></Header>
+    <!-- <div class="header">
+      i am header;
+    </div> -->
+    <div class="tab">
+      i am tab;
+    </div>
+    <div class="content">
+      i am content;
+
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Header from './components/header/header.vue'
+  export default {
+    components:{
+      Header
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -1,5 +1,7 @@
 <template lang="html">
-  
+  <div class="header">
+    ggggggggggggg
+  </div>
 </template>
 
 <script>
