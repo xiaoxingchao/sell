51c8cb4c7adebaf9e19ebf109f216ab19099b0d3
xxc <549021324@qq.com>
2017-08-23 23:30:10 +0800

    弹层

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -13,13 +13,31 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div class="support" v-if="seller.supports">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper">
-
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" alt="" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -30,6 +48,19 @@ export default {
     seller:{
 
     }
+  },
+  data(){
+    return {
+      detailShow:false
+    }
+  },
+  methods:{
+    showDetail(){
+      this.detailShow = true;
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
   }
 }
 </script>
@@ -37,16 +68,21 @@ export default {
 <style lang="stylus">
   @import "../../common/stylus/mixin";
   #header
-    color:#fff
-    background:#000
+    position: relative
+    overflow: hidden
+    color: #fff
+    background: rgba(7, 17, 27, 0.5)
     .content-wrapper
+      position: relative
       padding:24px 12px 18px 24px
       font-size:0
       .avatar
+        vertical-align:top
         display:inline-block
+        img
+          border-radius: 2px
       .content
         display:inline-block
-        font-size:14px
         margin-left:16px
         .title
           margin:2px 0 8px 0
@@ -63,5 +99,108 @@ export default {
             font-size:16px
             line-height:18px
             font-weight:bold
-
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .support
+          .icon
+            display: inline-block
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height: 12px
+            font-size: 10px
+      .support-count
+        position:absolute
+        right:12px
+        bottom:14px
+        padding:0 8px
+        height:24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0,0,0,0.2)
+        text-align: center
+        .count
+          vertical-align: top
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          line-height: 24px
+          font-size:10px
+          margin-right:2px
+    .bulletin-wrapper
+      position: relative
+      height:28px
+      line-height: 28px
+      padding:0 22px 0 12px
+      white-space:nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7,17,27,0.2)
+      .bulletin-title
+        vertical-align: top
+        margin-top:8px
+        display: inline-block
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        font-size: 10px
+        margin:0 4px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+    .background
+      position: absolute
+      top:0
+      left:0
+      width:100%
+      height:100%
+      z-index: -1
+      filter:blur(10px)
+    .detail
+      position: fixed
+      top: 0
+      left: 0
+      z-index: 100
+      width:100%
+      height:100%
+      overflow: auto
+      background: rgba(7,17,27,0.8)
+      .detail-wrapper
+        min-height: 100%
+        width:100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+      .detail-close
+        position: relative
+        width:32px
+        height:32px
+        margin:-64px auto 0 auto
+        clear:both
+        font-size: 30px
 </style>
