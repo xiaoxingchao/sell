7a7444b37ed7f5e64a40fd48d07e1777845df4c4
xxc <549021324@qq.com>
2017-08-24 07:04:46 +0800

    去掉多余

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
-    <!-- <div class="header">
-      i am header;
-    </div> -->
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
